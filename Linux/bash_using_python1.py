d714c2f02b214db1437070746d899a6275f88278
anjujosephm <99959654+anjujosephm@users.noreply.github.com>
2022-03-14 11:18:57 +0530

    Create bash_using_python1.py

---
+++ Linux/bash_using_python1.py
@@ -0,0 +1,4 @@
+# subprocess           : it is the built-in python module to perform bash using python
+
+import subprocess
+subprocess.run(['ls'])       : bash command to list all files and directories
